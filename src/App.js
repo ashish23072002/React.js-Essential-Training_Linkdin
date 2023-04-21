71f2b452a0b6b726e96eb807b968157baf9b72ef
ASHISH BISHT <ashish23072002@gmail.com>
2023-04-21 18:02:44 +0530

    chaper 5 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const [firstcity,] =["Delhi","Mumbai","Pune"]
+
 function App() {
+
+  const what=useState();
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello Froom React {firstcity} .
     </div>
   );
 }
